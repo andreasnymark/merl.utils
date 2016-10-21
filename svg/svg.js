62a06627cd05754bb51d9065b3aa64d056b27249
Andreas Nymark <andreas@nymark.me>
2016-10-21 17:43:25 +0200

    Removed button event

--- svg/svg.js
+++ svg/svg.js
@@ -1,6 +1,6 @@
 /*
 *
-*  
+*
 *
 *
 */
@@ -14,7 +14,7 @@ merl.svg = ( function ( window, document ) {
 			selectSVG: '.js-svg',
 			ns: 'http://www.w3.org/2000/svg',
 			bgColor: 'rgb( 218, 215, 212 )',
-			opacity: .4,
+			opacity: 0.4,
 			boundary: {
 				narrow: 800
 			},
@@ -25,13 +25,13 @@ merl.svg = ( function ( window, document ) {
 			}
 		},
 		instances = [],
-		docSize = { 
-			height: 0, 
+		docSize = {
+			height: 0,
 			width: 0,
 			offScreen: 0
 		};
-	
-	
+
+
 	/*
 	* @constructor SVG
 	*
@@ -46,14 +46,14 @@ merl.svg = ( function ( window, document ) {
 		s.running = false;
 
 		for ( var i = 0; i < 3; i++ ) {
-			s.rects.push( createRectangle( 0, 60, ( docSize.width ), docSize.height, s.color ) )
-			s.elem.appendChild( s.rects[ i ] );	
+			s.rects.push( createRectangle( 0, 60, ( docSize.width ), docSize.height, s.color ) );
+			s.elem.appendChild( s.rects[ i ] );
 		}
-		
+
         s.resizeContainer();
 	};
-	
-	
+
+
 	/*
 	* @method resizeContainer
 	*
@@ -66,9 +66,9 @@ merl.svg = ( function ( window, document ) {
         s.elem.setAttribute( 'viewBox', '0 0 ' + docSize.width + ' ' + docSize.height );
 		s.resetRects();
 	};
-    
 
-	
+
+
 	/*
 	* @method resetRects
 	*
@@ -76,13 +76,13 @@ merl.svg = ( function ( window, document ) {
 	*/
 	SVG.prototype.resetRects = function () {
 		var s = this,
-			rect0X = -docSize.offScreen - 100, 
+			rect0X = -docSize.offScreen - 100,
 			rect0Y = 160,
-			rect1X = -docSize.offScreen - 40, 
+			rect1X = -docSize.offScreen - 40,
 			rect1Y = 100,
-			rect2X = -docSize.offScreen, 
+			rect2X = -docSize.offScreen,
 			rect2Y = 60;
-		
+
 		if ( docSize.width < defs.boundary.narrow ) {
 			rect0X = -docSize.offScreen - 45;
 			rect0Y = 105;
@@ -90,25 +90,25 @@ merl.svg = ( function ( window, document ) {
 			rect1Y = 75;
 			rect2X = -docSize.offScreen;
 			rect2Y = 60;
-		} 
-		
+		}
+
 		s.rects[ 0 ].setAttributeNS( null, 'x', rect0X );
 		s.rects[ 0 ].setAttributeNS( null, 'y', rect0Y );
 		s.rects[ 0 ].setAttributeNS( null, 'width', docSize.width );
 		s.rects[ 0 ].setAttributeNS( null, 'opacity', '0' );
-		
+
 		s.rects[ 1 ].setAttributeNS( null, 'x', rect1X );
 		s.rects[ 1 ].setAttributeNS( null, 'y', rect1Y );
 		s.rects[ 1 ].setAttributeNS( null, 'width', docSize.width );
 		s.rects[ 1 ].setAttributeNS( null, 'opacity', defs.opacity );
-		
+
 		s.rects[ 2 ].setAttributeNS( null, 'x', rect2X );
 		s.rects[ 2 ].setAttributeNS( null, 'y', rect2Y );
 		s.rects[ 2 ].setAttributeNS( null, 'width', docSize.width );
-		s.rects[ 2 ].setAttributeNS( null, 'opacity', defs.opacity );	
+		s.rects[ 2 ].setAttributeNS( null, 'opacity', defs.opacity );
 	};
-	
-	
+
+
 	/*
 	* @method pseudoZoomIn
 	*
@@ -116,15 +116,19 @@ merl.svg = ( function ( window, document ) {
 	*/
 	SVG.prototype.pseudoZoomIn = function () {
 		var s = this;
-		
+
 		if ( !s.running ) {
 			s.running = true;
 			s.resetRects();
-			
-			s.rects[ 0 ].classList.add( defs.cls.in );
-			s.rects[ 1 ].classList.add( defs.cls.up );
-			s.rects[ 2 ].classList.add( defs.cls.out );	
-			
+
+//			s.rects[ 0 ].classList.add( defs.cls.in );
+//			s.rects[ 1 ].classList.add( defs.cls.up );
+//			s.rects[ 2 ].classList.add( defs.cls.out );
+
+			merl.utils.addClass( s.rects[ 0 ], defs.cls.in );
+			merl.utils.addClass( s.rects[ 1 ], defs.cls.up );
+			merl.utils.addClass( s.rects[ 2 ], defs.cls.out );
+
 			// Maybe do something else?
 			var t = setTimeout( ( function () {
 				var s = this;
@@ -133,14 +137,14 @@ merl.svg = ( function ( window, document ) {
 					s.rects[ 1 ].classList.remove( defs.cls.up );
 					s.rects[ 2 ].classList.remove( defs.cls.out );
 					s.running = false;
-					
+
 					//temp
-					document.querySelector( '.Outline-content' ).classList.remove( 'contentOut' );
+					// document.querySelector( '.Outline-content' ).classList.remove( 'contentOut' );
 				}
 			} ).bind( this ), 1600 );
 		}
 	};
-	
+
 
     /*
 	* @method sizeDoc
@@ -152,26 +156,26 @@ merl.svg = ( function ( window, document ) {
     var sizeDoc = function () {
 		var offScreen = 0.18,
 			obj;
-		
+
 		if ( document.body.clientWidth < defs.boundary.narrow ) offScreen = 0.05;
-        
+
 		obj = {
             width: document.body.clientWidth,
             height: document.body.clientHeight,
 			offScreen: Math.floor( document.body.clientWidth * offScreen )
         };
-        
+
         if( obj.height < window.innerHeight ) {
             obj.height = window.innerHeight;
-        } 
-        
+        }
+
         return obj;
-    }
-    
-	
+    };
+
+
     /*
 	* @method createRectangle
-	* 
+	*
 	* @param x {Number} x-position
 	* @param y {Number} y-position
 	* @param width {Number} width of rectangle
@@ -179,7 +183,7 @@ merl.svg = ( function ( window, document ) {
 	* @param color {String} color
 	* @param cls {String} classes to add to rectanggle
 	* return SVG {DOMObject} rectangle
-	*/	
+	*/
 	var createRectangle = function ( x, y, width, height, color, cls ) {
         var rect = document.createElementNS( defs.ns, 'rect' );
         rect.setAttributeNS( null, 'x', x );
@@ -189,14 +193,14 @@ merl.svg = ( function ( window, document ) {
         rect.setAttributeNS( null, 'fill', color );
 
 		if( cls && typeof cls === 'string' ) rect.classList.add( cls );
-		
+
 		return rect;
 	};
-	
-	
-	
-	
-	
+
+
+
+
+
     /*
 	* @method resizeRectangles
 	*
@@ -208,69 +212,73 @@ merl.svg = ( function ( window, document ) {
 			instances[ i ].resizeContainer();
 		}
 	};
-	
-	
-	
-	
+
+
+
+
+
+	var lorem = function ( evt ) {
+		for ( var i = 0, len = instances.length; i < len; i++ ) {
+			instances[ i ].pseudoZoomIn();
+		}
+
+		var t = setTimeout( function() {
+			window.scroll(0,0);
+		}, 1400 );
+
+		evt.preventDefault();
+	};
+
+
 	/*
 	* Init
-	* 
+	*
 	* @method init
 	* @param options {Object} Object
 	*/
 	var init = function ( options ) {
 		if( options ) {
-			for( var o in options ) { 
-				defaults[ o ] = options[ o ]; 
+			for( var o in options ) {
+				defaults[ o ] = options[ o ];
 			}
 		}
-        
+
 		var elems = document.querySelectorAll( defs.selectSVG );
-		
+
 		instances = [];
         docSize = sizeDoc();
-		
-		for ( var i = 0, len = elems.length; i<len; i++ ) {
+
+		for ( var i = 0, iLen = elems.length; i<iLen; i++ ) {
 			instances.push( new SVG( elems[ i ] ) );
 			instances[ i ].resetRects();
 		}
 
 		window.removeEventListener( 'resize', resizeRectangles );
 		window.addEventListener( 'resize', resizeRectangles );
-		
+
 		// temp
 //		document.querySelector( '.btnPseudoZoom' ).addEventListener( 'click', function () {
 //			console.log( 'Pseudo zoom!' );
 //			instances[ 0 ].pseudoZoomIn();
-//			
+//
 //			document.querySelector( '.Outline-content' ).classList.add( 'contentOut' );
-//			
+//
 //		});
-		
-		var navi = document.querySelectorAll( '.nav' );
-		for ( var i = 0, len = navi.length; i < len; i++ ) {
-			navi[ i ].addEventListener( 'click', function ( evt ) {
-				instances[ 0 ].pseudoZoomIn();
-				document.querySelector( '.Outline-content' ).classList.add( 'contentOut' );
-				
-				var t = setTimeout(function() {
-					window.scroll(0,0);
-				}, 1400 );
-				evt.preventDefault();
-			});
-		}
-		
-	};
-	
 
-	
-	
-    
-   
+
+
+	};
+
+
+
+
+
+
 	return {
 		init: init,
-		resizeRectangles: resizeRectangles
+		resizeRectangles: resizeRectangles,
+		lorem: lorem,
 	};
-   
-   
+
+
 }( window, document ));
