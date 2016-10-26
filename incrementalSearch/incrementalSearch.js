b0f27f45c692d83ec118380840c8943394907f6e
Andreas Nymark <andreas@nymark.me>
2016-10-26 21:25:04 +0200

    Boya! Clean up, BYO-JSON!, better structure of defaults, added Merl.utils.

--- incrementalSearch/incrementalSearch.js
+++ incrementalSearch/incrementalSearch.js
@@ -1,259 +1,266 @@
 /**
-* incrementalSearch.js
-* Real-time suggestions while searching, using XMLHttpRequest. 
-*
-* Author: Andreas Nymark <andreas@nymark.me>
-* License: MIT
-* 
-*/
+ * incrementalSearch.js
+ * Real time incremental search, using XMLHttpRequest and JSON result.
+ *
+ * @author Andreas Nymark <andreas@nymark.me>
+ * @license MIT
+ * @version 1
+**/
 var merl = merl || {};
 
 merl.incrementalSearch = ( function( window, document ) {
-	"use strict";
+	'use strict';
 
 
-	var defs = {
-		all: [],
-		input: '.js-incrementalsearch',
-		selectResult: '.Search-result',
-		expanded: 'is-expanded',
-		error: 'Something seems to be wrong with the server. Try to refresh, and let’s hope …',
-		noResult: 'No result.',
-		max: 5,
-		xhrUrl: '/ajax/search/',
-		viewAll: 'View all',
-		classItem: 'List-item',
-		classAnchor: 'Overlay-link',
-		classWrap: 'Search'
-	};
-	
-	
-	var searchTimeout = null;
-	var searchAll = [];
-	
-	
+	var searchTimeout = null,
+		searchAll = [],
+		defs = {
+			all: [],
+			selectInput: '.js-incrementalsearch',
+			selectWrap: '.Search',
+			selectResult: '.Search-result',
+			minChars: 3,
+			maxResult: 5,
+			timeout: 500, //ms
+			urlXHR: 'search.json?q=', // JSON result
+			urlPage: '/search?q=', // Search term will be added, encoded.
+			text: {
+				error: 'Something seems to be wrong with the server. Try to refresh, and let’s hope …',
+				more: 'View all',
+				empty: 'No result.',
+			},
+			tmpl: {
+				item: {
+					elem: 'div',
+					class: 'Search-item'
+				},
+				anchor: {
+					class: 'Link'
+				}
+			}
+		};
+
+
 	/**
-	* Contructor IncrementalSearch
-	*
-	* @constructor Toggle
-	* @param parent {Object} 
-	* @param event {Object} Event on handle  
-	* @param handle {Object} Handler of toggle
-	*/
+	 * @constructor IncrementalSearch
+	 * @param {HTMLElement} Element
+	**/
 	var IncrementalSearch = function( input ) {
 		var t = this;
-		t.input = input;
-		t.result = document.querySelector( defs.selectResult );
+		t.input = t.confirmInput( input );
+		t.result = t.resultList();
 		t.xhrSearch = new XMLHttpRequest();
-		
-		t.input.addEventListener( 'input', triggerSearch.bind( t ) );
+		t.input.addEventListener( 'input', this.triggerSearch.bind( t ) );
 		window.addEventListener( 'keydown', traverseResults.bind( t ) );
-		
 		t.xhrSearch.addEventListener( 'readystatechange', xhrState.bind( t ) );
 	};
 
-	
-	/*
-	* Init
-	* 
-	* @method init
-	* @param options {Object} Object
-	*/
-	var init = function( options ) {
-		if( options ) {
-			for( var o in options ) { 
-				defaults[ o ] = options[ o ]; 
-			}
-		}
-		defs.all = document.querySelectorAll( defs.input );
-		for( var i = 0, len = defs.all.length; i<len; i++ ) {
-			searchAll[i] = new IncrementalSearch( defs.all[ i ] );
-		} 
-	};
-	
 
-	
-	var xhrRequest = function( query, xhr, url ) {
-		var url = url + encodeURI( query );
-		xhr.open( 'GET', url, true );
+	IncrementalSearch.prototype = {
+
+		triggerSearch: function () {
+			clearTimeout( searchTimeout );
+			// 500ms delay on input so we don’t go crazy
+			searchTimeout = setTimeout( ( function () {
+				if ( this.input.value.length >= defs.minChars ) {
+					xhrRequest( this.input.value, this.xhrSearch, defs.urlXHR );
+				} else {
+					this.wipeResult();
+				}
+			}).bind( this ), defs.timeout );
+		},
+
+
+		/**
+		 * Loop and append search result
+		 *
+		 * @method appendResult
+		 * @param json {json}
+		**/
+		appendResult: function( json ) {
+			var data = JSON.parse( json ),
+				len = data.length,
+				r = this.result;
+
+			if( data.error ) {
+				r.innerHTML = data.error;
+				return;
+			}
+
+			r.innerHTML = '';
+
+			if( len > 0 ) {
+
+				for ( var i = 0; i < len && i < defs.maxResult; i++ ) {
+					r.appendChild( markupSearchItem( data[ i ].title, data[ i ].url ) );
+
+					// add view all if more than max
+					if( i + 1 === defs.maxResult ) {
+						r.appendChild( markupSearchItem( defs.text.more, defs.urlPage + encodeURI( this.input.value ) ) );
+					}
+				}
+			} else {
+				r.appendChild( markupSearchItem( defs.text.empty, null ) );
+			}
+		},
+
+
+		/**
+		 * Removes all HTML in 'result'
+		 *
+		 * @method wipeResult
+		**/
+		wipeResult: function () {
+			this.result.innerHTML = '';
+		},
+
+
+		/**
+		 * Adds a container for search result if none exist.
+		 *
+		 * @method resultList
+		 * @return {HTMLElement} Element - A ul element, appended after input.
+		**/
+		resultList: function () {
+			var res = document.querySelector( defs.selectResult );
+			if( !res ) {
+				res = document.createElement( 'div' );
+				res.classList.add( defs.selectResult.substr( 1 ) );
+				this.input.parentNode.appendChild( res );
+			}
+			res.setAttribute( 'aria-live', 'polite' );
+			return res;
+		},
+
+
+		/**
+		 * Make sure it’s an input element.
+		 *
+		 * @method confirmInput
+		 * @param {HTMLElement} Element - The input element
+		**/
+		confirmInput: function ( elem ) {
+			if ( elem.nodeName !== 'INPUT' ) {
+				elem = elem.querySelector( 'input[type=text]' );
+				if ( !elem ) console.log( 'incrementalSearch needs an input=text to work' );
+			}
+			return elem;
+		},
+	};
+
+
+	var xhrRequest = function ( query, xhr, url ) {
+		var newurl = url + encodeURI( query );
+		xhr.open( 'GET', newurl, true );
 		xhr.setRequestHeader( 'X-Requested-With', 'XMLHttpRequest' );
 		xhr.send();
 	};
-	
-	
-	
-	
-	var xhrState = function( evt ) {
+
+
+	var xhrState = function ( evt ) {
 		var xhr = evt.target;
 		if ( xhr.readyState === XMLHttpRequest.DONE ) {
 			if( xhr.status === 200 ){
 				this.appendResult( this.xhrSearch.responseText );
 			} else {
-				alert( defs.error );
+				alert( defs.text.error );
 			}
 		}
-	}
-	
-	
-	
-	
-	
-	var triggerSearch = function() {
-		clearTimeout( searchTimeout );
-
-		// 500ms delay on input so we don’t go crazy
-		searchTimeout = setTimeout( (function() { 
-			if ( this.input.value.length > 2 ) {
-				xhrRequest( this.input.value, this.xhrSearch, defs.xhrUrl );
-			} else {
-				this.wipeResult();
-			}
-		}).bind( this ), 500 ); 
 	};
-	
-	
-	
-	
-	
+
+
 	/**
-	* Jump between input and search results via arrows.
-	*
-	* @method traverseResults
-	* @param evt {Object} Key down event
-	*/
+	 * Template for each search result
+	 *
+	 * @method markupSearchItem
+	 * @param {String} title - title of search result
+	 * @param {String} url - URL to search result
+	 * @return {HTMLElement} Element - as a li
+	**/
+	var markupSearchItem = function( title, url ) {
+		var item = document.createElement( defs.tmpl.item.elem );
+		if ( url ) {
+			var anchor = document.createElement( 'a' );
+			anchor.classList.add( defs.tmpl.anchor.class );
+			anchor.setAttribute( 'href', encodeURI( url ) );
+			anchor.textContent = title;
+			item.appendChild( anchor );
+		} else {
+			item.textContent = title;
+		}
+		item.classList.add( defs.tmpl.item.class );
+		return item;
+	};
+
+
+	/**
+	 * Jump between input and each search result item using arrow keys.
+	 *
+	 * @method traverseResults
+	 * @param {KeyEvent} KeyEvent
+	**/
 	var traverseResults = function( evt ) {
-		var elemActive = document.activeElement; 
+		var elemActive = document.activeElement;
 
 		// make sure we disable default key up/down only when search is open.
-		if( parentsUntilClass( elemActive, defs.classWrap ) ) {
+		if( merl.utils.parentUntilClass( elemActive, defs.selectWrap ) ) {
 			var nextSibling = elemActive.parentElement.nextElementSibling;
 			var prevSibling = elemActive.parentElement.previousElementSibling;
-			
+
 			// key down
-			if ( evt.keyCode == 40 ) {  
-				if( this.input == elemActive ) {
+			if ( evt.keyCode === 40 ) {
+				if ( this.input === elemActive ) {
 					var first = this.result.firstChild.firstChild || null;
-					if( first != null ) first.focus();
-				} else if( nextSibling != null ) {
-					nextSibling.firstChild.focus(); 
+					if ( first !== null ) first.focus();
+				} else if ( nextSibling !== null ) {
+					nextSibling.firstChild.focus();
 				}
 				evt.preventDefault();
 			}
-			
+
 			// key up
-			if ( evt.keyCode == 38 ) {      
-				if( this.input != elemActive && prevSibling != null ) {
-					prevSibling.firstChild.focus(); 
-				} else if( this.input != document.activeElement ) {
+			if ( evt.keyCode === 38 ) {
+				if ( this.input !== elemActive && prevSibling !== null ) {
+					prevSibling.firstChild.focus();
+				} else if ( this.input !== document.activeElement ) {
 					this.input.focus();
 				}
 				evt.preventDefault();
 			}
-			
+
 		}
 
 	};
-	
-	
-	
-	/**
-	* Template for each search result
-	*
-	* @method markupSearchItem
-	* @param title {String} Title
-	* @param url {String} Url
-	* return item {DOMobject} DOMObject
-	*/
-	var markupSearchItem = function( title, url ) {
-		var item = document.createElement( 'li' );
-		if ( url ) {
-			var anchor = document.createElement( 'a' );
-			anchor.classList.add( defs.classAnchor );
-			anchor.setAttribute( 'href', url );	
-			anchor.textContent = title;
-			item.appendChild( anchor );
-		} else {
-			item.textContent = title;	
-		}
-		item.classList.add( defs.classItem );
-		return item;
-	};
-	
-	
-	
-	/**
-	* Loop and append search result
-	*
-	* @method appendResult
-	* @param json {json} 
-	*/
-	IncrementalSearch.prototype.appendResult = function( json ) {
-		var data = JSON.parse( json );
-		var len = data.length;
-		
-		if( data.error ) {
-			this.result.innerHTML = data.error;	
-			return;
-		}
-		
-		// empty
-		this.result.innerHTML = '';
-		
-		if( len > 0 ) {
-			for (var i = 0; i<len; i++) {
-				var url = data[ i ][ 'url' ];
-				var title = data[ i ][ 'title' ];		
-				this.result.appendChild( markupSearchItem( title, url ) );
 
-				// add view all if more than max
-				if( i + 1 == defs.max ) {
-					this.result.appendChild( markupSearchItem( defs.viewAll, '/search?q=' + encodeURI( this.input.value ) ) );
-				}
-			}
-		} else {
-			this.result.appendChild( markupSearchItem( defs.noResult, null ) );
-		}
-		
-		
-	};
 
-	
-	
 	/**
-	* Removes all HTML in 'result'
-	*
-	* @method wipeResult
-	*/
-	IncrementalSearch.prototype.wipeResult = function() {
-		this.result.innerHTML = '';
-	};
-
-		
-	
-	
-	/**
-	* Traverse DOM upwards until class
-	*
-	* @method parentsUntilClass
-	* @param el {Object} Element to start from
-	* @param cls {String} Class name where to stop
-	* @return {Object}
-	*/
-	var parentsUntilClass = function ( el, cls ) {
-		while ( el.parentNode ) {
-			el = el.parentNode; 
-			if ( el.nodeType == 1 && el.getAttribute( 'class' ) && el.classList.contains( cls ) ) {
-				return el;
+	 * Init
+	 *
+	 * @method init
+	 * @param options {Object} Object
+	**/
+	var init = function( options ) {
+		if ( options ) {
+			for ( var o in options ) {
+				defs[ o ] = options[ o ];
 			}
 		}
-		return null;
+
+		// Include
+		if ( !merl.utils ) console.log( 'incrementalSearch requires merl.utils.js' );
+
+		defs.all = document.querySelectorAll( defs.selectInput );
+
+		if ( defs.all.length > 0 ) {
+			for( var i = 0, len = defs.all.length; i<len; i++ ) {
+				searchAll[i] = new IncrementalSearch( defs.all[ i ] );
+			}
+		}
 	};
-	
-	
-   
+
+
 	return {
 		init: init
 	};
-   
-   
+
 }( window, document ));
