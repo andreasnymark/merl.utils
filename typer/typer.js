1be56026625c866a8baa6e7a52a7539d250d2a7a
Andreas Nymark <andreas@nymark.me>
2019-05-04 17:09:09 +0200

    Work in progress. Clean up.

--- typer/typer.js
+++ typer/typer.js
@@ -1,5 +1,6 @@
 /**
- * A little …
+ * Typer is a small, basic type testing tool using sample 
+ * texts and the most core styling capabilities. 
 
  * @author Andreas Nymark <andreas@nymark.me>
  * @license MIT
@@ -13,11 +14,17 @@ merl.typer = ( function ( window, document ) {
 	var instances = [];
 	var defs = {
 		selector: '.js-typer',
+		selectPreivew: '.Typer-preview',
+		selectInput: '[data-typer-prop]',
+		attrSync: 'data-typer-sync',
+		attrProperty: 'data-typer-prop',
 		sample: {
 			'sv-h': 'Rubrik på svenska',
 			'sv-p': 'Det kom en visshet över honom, medan han stod där, att han hade idel fiender i kyrkan, fiender i alla bänkar. Bland herrskaperna på läktaren, bland bönderna nere i kyrkan, bland nattvardsbarnen i koret hade han fiender, idel fiender. Det var en fiende, som trampade orgeln, en fiende, som spelade den. I kyrkovärdarnas bänk hade han fiender. Alla hatade honom, alltifrån de små barnen, som hade burits in i kyrkan, ända till kyrkvaktaren, en stel och styv soldat, som hade varit med vid Leipzig. ',
-			'en-p': 'English',
-			'fr-p': 'French',
+			'en-h': 'English headline',
+			'en-p': 'Alice was beginning to get very tired of sitting by her sister on the bank and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice, “without pictures or conversations?” — So she was considering, in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trou- ble of getting up and picking the daisies.',
+			'fr-h': 'French headline',
+			'fr-p': 'Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire: «Je m’endors.» Et, une demi-heure après, la pen- sée qu’il était temps de chercher le sommeil m’éveillait; je voulais poser le volume que je croyais avoir encore dans les mains et souffler ma lumière.',
 		}
 	};
 
@@ -43,7 +50,6 @@ merl.typer = ( function ( window, document ) {
 			for ( var i = 0; i < len; i++ ) {
 				instances.push( new Typer( items[ i ] ) );
 			}
-			// window.addEventListener( 'resize', measureWinSize );
 		}
 	};
 
@@ -59,41 +65,67 @@ merl.typer = ( function ( window, document ) {
 
 		var t = this;
 		t.elem = elem;
-		t.elemText = t.elem.querySelector( '.Typer-text' );
+		t.hasCustom = false;
 		t.customText = '';
-		t.settings = t.elem.querySelectorAll( '[data-typer-prop]' );
 
-	
+		t.inputs = t.elem.querySelectorAll( defs.selectInput );
+		t.elemText = t.elem.querySelector( defs.selectPreivew );
+		/* Handle this */ t.sampleSelect = t.elem.querySelector( '#sample_tt1' );
 
-		
-
-		
-		t.paste = t.paste.bind( t );
-		t.input = t.input.bind( t );
-		t.change = t.change.bind( t );
+		t.update = t.update.bind( t );
+		t.addCustom = t.addCustom.bind( t );
 		t.updateText = t.updateText.bind( t );
+		t.removeLang = t.removeLang.bind( t );
+		
 		t.updateStyle = t.updateStyle.bind( t );
+		t.pasteRawText = t.pasteRawText.bind( t );
+		t.inputCustomText = t.inputCustomText.bind( t );
 
-
-		for ( var i = 0, len = t.settings.length; i < len; i++ ) {
-			t.settings[ i ].addEventListener( 'input', t.change );
+		for ( var i = 0, len = t.inputs.length; i < len; i++ ) {
+			t.inputs[ i ].addEventListener( 'input', t.update );
 		}
-
-		t.elemText.addEventListener( 'paste', t.paste );
-		t.elemText.addEventListener( 'input', t.input );
-
+		t.elemText.addEventListener( 'paste', t.pasteRawText );
+		t.elemText.addEventListener( 'input', t.inputCustomText );
 	};
 
 
 	Typer.prototype = {
+		/**
+		 * Core Type update event
+
+		 * @method update
+		 * @param evt { Object } Event
+		**/
+		update: function ( evt ) {
+			if ( evt ) {
+				var t = this;
+				var val = evt.target.value;
+				var prop = evt.target.getAttribute( defs.attrProperty );
+				var sync = evt.target.getAttribute( defs.attrSync ) || false;
+				
+				if ( prop === 'content' ) {
+					if ( val === 'custom' ) {
+						t.updateText( t.customText );
+					} else {
+						t.updateText( defs.sample[ val ] );	
+					}
+				} else if ( prop === 'font-size' ) {
+					t.updateStyle( prop, val + 'px' );
+				} else {
+					t.updateStyle( prop, val );
+				}
+
+				if ( sync ) t.elem.querySelector( sync ).value = val;
+			}
+		},
 
 		/**
 		 * Just pasting clean text, no HTML.
 
-		 * @method paste
+		 * @method pasteRawText
 		 * @param evt {Object} Event
 		**/
-		paste: function ( evt ) {
+		pasteRawText: function ( evt ) {
 			var t = this;
 			var c = t.customText;
 
@@ -108,45 +140,61 @@ merl.typer = ( function ( window, document ) {
 		},
 
 		/**
-		 * Change styles of content of text element.
-
-		 * @method change
-		 * @param evt {Object} Event
+		 * Remove lang attribute, since we can’t 
+		 * tell every time.
 		**/
-		change: function ( evt ) {
-			if ( evt ) {
-				var t = this;
-				var prop = evt.target.getAttribute( 'data-typer-prop' );
-				var val = evt.target.value;
-				
-				if ( prop === 'content' ) {
-					if ( val === 'custom' ) {
-						t.updateText( t.customText );
-					} else {
-						t.updateText( defs.sample[ val ] );	
-					}
-				} else if ( prop === 'font-size' ) {
-					t.updateStyle( prop, val + 'px' );
-				} else {
-					t.updateStyle( prop, val );
-				}
-			}
+		removeLang: function () {
+			this.elemText.removeAttribute( 'lang' );
 		},
 
-
-
-		input: function ( evt ) {
+		/**
+		 * Create element and add custom option 
+		 * in sample select.
+		**/
+		addCustom: function () {
 			var t = this;
+			var grp = document.createElement( 'optgroup' );
+			var opt = document.createElement( 'option' );
 
+			opt.value = 'custom';
+			opt.innerHTML = 'Custom';
+			
+			grp.label = 'Other';
+			grp.appendChild( opt );
+
+			t.sampleSelect.appendChild( grp );
+		},
+
+		/**
+		 * When user add/change sample text, we create and select 
+		 * a custom option in select. Latest edit will always be 
+		 * available to use again.
+		**/
+		inputCustomText: function ( evt ) {
+			var t = this;
 			t.customText = evt.target.innerText;
-	// Set to custom
-	// var opts = sample.options;
-	// for ( var opt, i = 0; opt = opts[ i ]; i++ ) {
-	// 	if ( opt.value == 'custom' ) {
-	// 		sample.selectedIndex = i;
-	// 		break;
-	// 	}
-	// }
+			if ( ! t.hasCustom ) {
+				t.hasCustom = true;
+				t.addCustom();
+			}
+			t.activateCustom();
+		},
+
+		/**
+		 * Set to custom option in sample select. 
+		**/
+		activateCustom: function () {
+			var t = this;
+			if ( t.hasCustom ) {
+				var sample = t.sampleSelect;
+				var opts = sample.options;
+				for ( var opt, i = 0; opt = opts[ i ]; i++ ) {
+					if ( opt.value == 'custom' ) {
+						sample.selectedIndex = i;
+						break;
+					}
+				}
+			}
 		},
 
 		/**
@@ -159,6 +207,7 @@ merl.typer = ( function ( window, document ) {
 			var t = this;
 			try {
 				t.elemText.innerText = txt;	
+				t.removeLang();
 			} catch ( err ) {
 				alert( err.name + ' ' + err.message );
 			}
@@ -180,10 +229,7 @@ merl.typer = ( function ( window, document ) {
 				alert( err.name + ' ' + err.message );
 			}
 		},
-
 	};
-
-
 
 	return {
 		init: init,
